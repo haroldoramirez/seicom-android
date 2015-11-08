b0e0d834bd5f926d0534ea488fb88860c9d61553
Thiago Medeiros <thsouzza@gmail.com>
2015-11-08 16:33:21 -0200

    updating exercicio02

--- exercicios/exer-02/FormularioActivity.java
+++ exercicios/exer-02/FormularioActivity.java
@@ -5,17 +5,16 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class FormularioActivity extends AppCompatActivity {
+public class ListaContatosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formulario);
+        setContentView(R.layout.activity_lista_contatos);
 
         String[] contatos = {"Thiago", "João", "Luciano"};
         ListView listaContatos = (ListView) findViewById(R.id.lista_contatos);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contatos);
         listaContatos.setAdapter(adapter);
-
     }
 }
