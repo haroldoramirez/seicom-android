8248197c778b45cde9312e69657907a1323194e1
Thiago Medeiros <thsouzza@gmail.com>
2015-11-10 15:29:47 -0200

    adding formularioActivity.java - ex06

---
+++ exercicios/exer-06/FormularioActivity.java
@@ -0,0 +1,51 @@
+package br.com.anglo.agenda;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.com.anglo.agenda.dao.ContatoDAO;
+import br.com.anglo.agenda.modelo.Contato;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        helper = new FormularioHelper(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Contato contato = helper.pegaContato();
+                ContatoDAO dao = new ContatoDAO(FormularioActivity.this);
+                dao.insere(contato);
+                dao.close();
+                Toast.makeText(FormularioActivity.this, "Contato " + contato.getNome() +  " salvo!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
